4c702a1f07cb0744965b1ae14e65f2214f6efad4
pacbac <jclaytonc99@gmail.com>
2019-01-26 03:25:14 -0800

    Add search program for drugcount data retrieval

--- scripts/statestats.py
+++ scripts/statestats.py
@@ -5,31 +5,21 @@ DATA_DIR = f'{os.getcwd()}/2019_MCMProblemC_DATA/MCM_NFLIS_Data.xlsx'
 wb = load_workbook(DATA_DIR)
 sheet = wb.worksheets[1]
 
-yrToCol = {
-    2010: 'a',
-    2011: 'b',
-    2012: 'c',
-    2013: 'd',
-    2014: 'e',
-    2015: 'f',
-    2016: 'g',
-    2017: 'h'
-}
+yrs = [ yr for yr in range(2010, 2018) ]
 
 def main():
     states = {'KY': {}, 'VA': {}, 'WV': {}, 'PA': {}, 'OH': {}}
     curYr = 2010
     statesWb = {'KY': Workbook(), 'VA': Workbook(), 'WV': Workbook(), 'PA': Workbook(), 'OH': Workbook()}
-    print(sheet.cell(row=1, column=1).value, sheet.cell(row=1, column=2).value, sheet.cell(row=1, column=3).value)
 
     for j in statesWb.keys():
         newSheet = statesWb[j].active
         newSheet.cell(row=1, column=1).value = 'County'
-        for yr in yrToCol.keys():
+        for yr in yrs:
             newSheet.cell(row=1, column=(yr-2008)).value = yr
         states[j]['counties'] = []
         states[j]['entries'] = {}
-        for yr in yrToCol.keys():
+        for yr in yrs:
             states[j]['entries'][yr] = []
 
     prevCounty = None
@@ -46,8 +36,6 @@ def main():
         elif prevCounty != county: # skip over repeating county entries
             tbl['entries'][yr].append((county, drugCount))
         prevCounty = county
-    
-    print(len(states['VA']['counties'])) 
 
     for state in states.keys():
         newSheet = statesWb[state].active
@@ -60,7 +48,3 @@ def main():
             for county, drugCount in entries[yr]:
                 newSheet.cell(row=counties.index(county)+2, column=(yr-2008)).value = drugCount
         statesWb[state].save(f'generated_data/{state}test.xlsx')
-
-
-if __name__ == "__main__":
-    main()
