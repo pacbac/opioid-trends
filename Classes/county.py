9dee118223e268f5b2bf93565882460677fed277
Tanishqbh <tanishqbh@gmail.com>
2019-01-26 14:39:32 -0800

    CountyClass update

--- Classes/county.py
+++ Classes/county.py
@@ -1,15 +1,40 @@
 import math
+import os, sys
+ROOT_WDIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.insert(0, ROOT_WDIR)
+from scripts.searchDrugCounty import search
 
+import csv
 
 class County: 
-    drugList = {}
-    lat = 0.0
-    lng = 0.0
+    m_name = ""
+    m_state = ""
+    m_drugList = {}
+    m_lat = 0.0
+    m_lng = 0.0
+    def __init__(self, nm,st,lt,ln):
+        self.m_name = nm
+        self.m_state = st
+        self.m_drugList = {}
+        self.m_lat = lt
+        self.m_lng = ln
+
     def distanceTo(self,otherCounty):
          return math.sqrt(
-        ((self.lat - otherCounty.lat)**2 + 
-        ((math.cos((self.lat - otherCounty.lat)/2)*
-        (self.lng-otherCounty.lng)**2))) )
+        ((self.m_lat - otherCounty.m_lat)**2 + 
+        ((math.cos((self.m_lat - otherCounty.m_lat)/2)*
+        (self.m_lng-otherCounty.m_lng)**2))) )
+
+
+
+allCounties = []
+with open(ROOT_WDIR +"/generated_data/latlng.csv") as cnties:
+      countyReader = csv.DictReader(cnties)
+
+      for cnty in countyReader:
+        [name,state] = cnty['County'].split(',')
+        allCounties.append(County(name,state,cnty['Latitude'],cnty['Longitude']))
+
     
 
     
